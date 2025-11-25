cf61f0229f32d2e4183607867fa92c0dd9ce8ec9
Joinside <43420583+Joinsider@users.noreply.github.com>
2025-11-25 17:09:43 +0100

    Update URL and SHA256 for version 1.0.1

--- Formula/sign-dhbw-next.rb
+++ Formula/sign-dhbw-next.rb
@@ -1,8 +1,8 @@
 class SignDhbwNext < Formula
   desc "DHBW signature tool"
   homepage "https://github.com/Joinsider/homebrew-apps"
-  url "https://github.com/Joinsider/homebrew-apps/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "822d5c3d51d0b36548c21f553a02655d7b2107795fde9cb5cb5a5e3ca0bb693b"
+  url "https://github.com/Joinsider/homebrew-apps/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "683fe54ea50cfc7c1776edb747ef78f9accaf810536329ee252af656cc2adc63"
   license "MIT"
 
   def install
