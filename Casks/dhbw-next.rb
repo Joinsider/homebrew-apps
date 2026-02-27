4f5de534831b460b2205491bb590a9d5a8d5a406
Joinside <43420583+Joinsider@users.noreply.github.com>
2026-02-27 20:08:39 +0100

    Update URL and homepage for dhbw-next cask

--- Casks/dhbw-next.rb
+++ Casks/dhbw-next.rb
@@ -2,10 +2,10 @@ cask "dhbw-next" do
   version "1.0.13"
   sha256 "523469d7239ebe1be6e89ab35e1bcec44c52eabed3d6418773a3b0e4db6ed560"
 
-  url "https://github.com/Joinsider/dhbw-next/releases/download/v#{version}/dhbw-next-#{version}.dmg"
+  url "https://github.com/Joinsider/dhbw/releases/download/v#{version}/dhbw-horb-student-app-#{version}.dmg"
   name "DHBW Next"
   desc "Student application for DHBW Horb"
-  homepage "https://github.com/Joinsider/dhbw-next"
+  homepage "https://github.com/Joinsider/dhbw"
 
   livecheck do
     url :url
